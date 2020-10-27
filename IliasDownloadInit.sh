97abb02c7b2cd6a8c1f5496975980540a78d04e6
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2020-10-28 00:53:24 +0100

    Small changes

--- IliasDownloadInit.sh
+++ IliasDownloadInit.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Modify directories at the end of this script
-
 # Enter ILIAS username and password here
+# Modify here
 ILIAS_USERNAME=mm1234s
-ILIAS_PASSWORD=passwort
+ILIAS_PASSWORD=password
 
 
 # Choose a relative path to store the file in every data directory or an absolute path for one single file
@@ -18,20 +17,35 @@ do_login
 
 # Insert the folders you want to fetch here.
 # Take the id of the folder out of the URL, e.g.
-# https://ilias3.uni-stuttgart.de/goto_Uni_Stuttgart_fold_1086607.html
-#                                                         ^^^^^^^
+# https://www.ili.fh-aachen.de/goto_elearning_crs_604137.html
+#                                                 ^^^^^^
+# or
+# https://www.ili.fh-aachen.de/goto_elearning_crs_604137.html
+#                                                 ^^^^^^
 # You find this link at the bottom of every folder page.
 # Subfolders are automatically downloaded, too.
 # You need to use absolute paths for local folders!
+#
+# You can download the hole course or just specific folders.
 
-# You might preset your home folder like this:
+# Preset your Homefolder
 MY_STUDIES_FOLDER="/E/IliasTest"
 
+# Copy for every course/folder you want to download
+# Modify here
+fetch_folder "593215" "$MY_STUDIES_FOLDER/Digitale Services für die Energie- und Mobolitätswende"
 
-# To download the a course with subfolders, add the type prefix, e.g.
-# https://ilias3.uni-stuttgart.de/goto_Uni_Stuttgart_crs_1417864.html
-#                                                    ^^^^^^^^^^^
-fetch_folder "604137" "$MY_STUDIES_FOLDER/Kurs1"
+fetch_folder "590654" "$MY_STUDIES_FOLDER/Energiedatenanalyse - statistische Methoden"
+
+fetch_folder "608226" "$MY_STUDIES_FOLDER/Geschäftsprozessmodellierung und betriebliche Informationssysteme"
+
+fetch_folder "604137" "$MY_STUDIES_FOLDER/Machine Learning 1 Grundlagen und statistische Lerntheorie"
+
+fetch_folder "590651" "$MY_STUDIES_FOLDER/Management flexibler Energiesysteme"
+
+fetch_folder "590657" "$MY_STUDIES_FOLDER/Management regenerativer Energiesysteme"
+
+fetch_folder "290557" "$MY_STUDIES_FOLDER/Verteilte Systeme - Grundlagen"
 
 # Don't modify
 do_logout
